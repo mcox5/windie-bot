3eade5340c5bd341ed07edd10b3b2c10247e7a4d
mcox5 <mcox5@uc.cl>
2023-12-03 16:16:06 -0300

    feat(sports): add sports table

---
+++ app/models/sport.rb
@@ -0,0 +1,12 @@
+class Sport < ApplicationRecord
+end
+
+# == Schema Information
+#
+# Table name: sports
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
