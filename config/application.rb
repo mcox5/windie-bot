b511fc40fa50ba3ac1e5195945e0c614cee88f9e
mcox5 <mcox5@uc.cl>
2023-12-03 13:17:48 -0300

    chore(): change app name

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,9 @@ require_relative "boot"
 
 require "rails/all"
 Bundler.require(*Rails.groups)
-require_relative '../lib/dotenv_monkeypatch'
+# require_relative '../lib/dotenv_monkeypatch'
 
-module MyApi
+module Windie
   class Application < Rails::Application
     config.middleware.insert_before 0, Rack::Cors do
       allow do
@@ -19,6 +19,7 @@ module MyApi
 
     config.before_configuration do
       require Rails.root.join('lib/environment_variables.rb')
+      require Rails.root.join('lib/windguru_locations.rb')
     end
     config.i18n.fallbacks = [:es, :en]
     config.i18n.default_locale = 'es-CL'
