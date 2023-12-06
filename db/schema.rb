aa4a8c4ac04192d96c1e1dd240d050a91d278f39
mcox5 <mcox5@uc.cl>
2023-12-06 20:38:43 -0300

    feat(user/AASM): add column in schema

--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_06_215345) do
     t.datetime "updated_at", null: false
     t.string "alias"
     t.string "phone"
+    t.string "registration_state"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
