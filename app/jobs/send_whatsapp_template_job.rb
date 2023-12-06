1f5664d8351ae1ec9ff231354b785828c709fc27
mcox5 <mcox5@uc.cl>
2023-12-06 20:20:28 -0300

    fix(whatsapp/Template): change class name to make more representative

--- app/jobs/send_whatsapp_template_job.rb
+++ app/jobs/send_whatsapp_template_job.rb
@@ -1,13 +1,13 @@
 require 'twilio-ruby'
 
-class SendWhatsappJob < ApplicationJob
+class SendWhatsappTemplateJob < ApplicationJob
   queue_as :default
 
   def perform(phone, content_sid, content_variables)
     @client = Twilio::REST::Client.new(TwilioConstants.account_sid, TwilioConstants.auth_token)
     message = @client.messages.create(
       content_sid: content_sid,
-      from: "whatsapp:#{TwilioConstants.phone_number}",
+      from: "whatsapp:#{TwilioConstants.sandbox_phone_number}",
       content_variables: content_variables.to_json,
       messaging_service_sid: TwilioConstants.messaging_service_sid,
       to: "whatsapp:#{phone}"
