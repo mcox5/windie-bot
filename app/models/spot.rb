c2207fc4fce1faeb591b0b8a96a4510417e15b01
mcox5 <mcox5@uc.cl>
2023-12-18 23:01:52 -0300

    feat(spot): create spot observer

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,7 @@
 class Spot < ApplicationRecord
+  include PowerTypes::Observable
+  include ActiveModel::Dirty
+
   MORNING_HOURS = ["01h", "02h", "03h", "04h", "05h", "06h", "07h", "08h", "09h", "10h", "11h", "12h", "13h"]
   AFTERNOON_HOURS = ["14h", "15h", "16h", "17h", "18h", "19h", "20h", "21h", "22h", "23h", "00h"]
 
---
+++ app/observers/spot_observer.rb
@@ -0,0 +1,35 @@
+class SpotObserver < PowerTypes::Observer
+  before_update :send_report_whatsapp
+
+  def send_report_whatsapp
+    return unless !object.today_report_summary["morning"].empty?
+
+    object.users.each do |user|
+      today_report_variables = today_report_variables(user.user_alias)
+      SendWhatsappTemplateJob.perform_now(user.phone, TwilioConstants.show_daily_report_template_sid, today_report_variables)
+    end
+  end
+
+  private
+
+  def today_report_variables(user_alias)
+    {
+      "1" => "hoy",
+      "2" => object.name,
+      "3" => object.today_report_summary["morning"]["wave"],
+      "4" => object.today_report_summary["morning"]["wind"],
+      "5" => object.today_report_summary["morning"]["wind_direction"],
+      "6" => object.today_report_summary["morning"]["period"],
+      "7" => object.today_report_summary["afternoon"]["wave"],
+      "8" => object.today_report_summary["afternoon"]["wind"],
+      "9" => object.today_report_summary["afternoon"]["wind_direction"],
+      "10" => object.today_report_summary["afternoon"]["period"],
+      "11" => object.today_report_summary["tides"]["Lo"][0],
+      "12" => object.today_report_summary["tides"]["Lo"][1],
+      "13" => object.today_report_summary["tides"]["Hi"][0],
+      "14" => object.today_report_summary["tides"]["Hi"][1],
+      "15" => user_alias,
+      "16" => object.windguru_code
+    }
+  end
+end
