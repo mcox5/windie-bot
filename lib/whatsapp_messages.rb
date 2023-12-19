85eb27a2cce7eaccf5e45d4bd6b41bcd80297e50
mcox5 <mcox5@uc.cl>
2023-12-18 23:03:37 -0300

    feat(whatsapp-messages): add new windie features message for beta version

--- lib/whatsapp_messages.rb
+++ lib/whatsapp_messages.rb
@@ -10,7 +10,28 @@ Me puedes *preguntar lo que quieras* relacionado al reporte de las olas 😯, ya
 🚨 Puedes *dejar programadas alertas* de cuando vengan *buenas olas* en las próximas semanas, para que puedas organizar con calma tu próximo viaje!\n
 *Ejemplo*: Avísame cuando vengan olas de más de 2 metros a Matanzas en los próximos 15 días\n\n
 Espero que te guste Windie🤖🌊💨! Cualquier duda o sugerencia, escríbeme a mi mail windie.chile@gmail.com o a mi WhatsApp: +56990168398\n\n
-Si Windie te es útil, puedes ayudarme a seguir mejorando compartiéndolo con tus amigos 🙏🏼\n\n"
+Si Windie te es útil, puedes ayudarme a seguir mejorando compartiéndolo con tus amigos 🙏🏼\n\n",
+      SHOW_WINDIE_FEATURES_BETA: "Genial! Ya estamos listos con el registro 👌🏼!\n\n
+⏰Todas las mañanas te llegará el *reporte diario* del spot que elegiste al registrarte!\n\n
+Ahora te voy a contar un poco sobre cómo puedes interactuar con *Windie*.\n\n
+Si quieres saber cómo estará el mar en tu spot favorito, puedes escribir los siguientes comandos\n\n
+*reporte de hoy*\n
+*reporte de mañana*\n
+*reporte de mareas hoy*\n
+*reporte de mareas mañana*\n
+*olas grandes en la proxima semana*\n\n
+
+Esta versión de Windie es una *versión beta* 🐣\n
+Próximamente tendrás las siguientes features disponibles:\n\n
+⏰ *Programar reportes diarios* para que te lleguen a la hora que quieras\n
+🚨 *Alertas de olas grandes* para que te avise cuando vengan buenas olas a tu spot favorito\n
+🌊 Poder consultar las condiciones de *cualquier spot* del mundo!\n
+🤖 *Windie IA* vas a poder entrenar e interactuar con windie con lenguaje natural y *NO* con comandos\n
+Es decir le podrás dar instrucciones para que te de alertas para condiciones específicas de cada spot o lo que se te ocurra!\n\n
+Espero que te guste Windie🤖🌊💨! Cualquier duda/sugerencia/feedback, escríbeme\n📪 *mail* windie.chile@gmail.com\n✆ *WhatsApp* *+56990168398*"
+    },
+    CONVERSATION: {
+      ERROR: "Lo siento, aún no estoy programado para responder ese tipo de mensajes 😕. Por favor, intenta de nuevo con uno de los comandos que te dije en el mensaje de bienvenida!",
     }
   }
 
