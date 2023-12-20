757f4a3bb9c191d68db11a1a58f1104cbd420a1d
mcox5 <mcox5@uc.cl>
2023-12-20 16:32:02 -0300

    debug(scheduler): delete sidekiq worker to Scrapping class

--- app/jobs/scrapping_windguru_job.rb
+++ app/jobs/scrapping_windguru_job.rb
@@ -1,7 +1,6 @@
 require 'nokogiri'
 require 'selenium-webdriver'
 require 'json'
-require 'sidekiq-scheduler'
 
 SPANISH_MONTHS = {
   'ene.' => 'Jan',
@@ -19,7 +18,6 @@ SPANISH_MONTHS = {
 }
 
 class ScrappingWindguruJob < ApplicationJob
-  include Sidekiq::Worker
   queue_as :default
 
   def perform(location_name)
